de9d73e5c37ce6186afa3a6f8e1868041c522ba6
jyyblue <jyyblue1987@outlook.com>
2021-04-06 22:21:18 +0800

    [add] open url from telegram bot message.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import asyncio
 import sys
 from telethon.sync import TelegramClient, events
+import re
+import webbrowser
 
 api_id = '3961096'
 api_hash = '5c814390e26776fbe56919f7bff872cc'
 channelId = 'https://t.me/testgrp_pi'
+
+api_id = 3743452
+api_hash = 'f65ae54bdbf263c003de4315b1165de8'
+channelId = 'https://t.me/MDXTrading'
+
+def findUrls(url):
+    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', url)
+    return urls
+
 
 class CLITelegram(TelegramClient):
     def __init__(self, session_user_id, api_id, api_hash, on_message):
@@ -29,7 +40,13 @@ class CLITelegram(TelegramClient):
 def on_message(message):
     print(message)
 
+    urls = findUrls(message)
+    for url in urls:
+        webbrowser.open(url, new=2)
+
+
 if __name__ == "__main__":
+
     client = CLITelegram('my_listener', api_id, api_hash, on_message)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(client.run())
